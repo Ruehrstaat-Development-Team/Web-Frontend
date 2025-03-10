ec1984fef2a62e19f9954f5c31c339a0220e124d
Sebastian Dolfen <sebi.dolfen.sd@gmail.com>
2025-03-11 00:25:34 +0100

    Refactor carrier card components to use I18nLink for routing; remove unused locale files

--- components/Page/Carrier/Card/PageCarrierCardPublic.vue
+++ components/Page/Carrier/Card/PageCarrierCardPublic.vue
@@ -1,28 +1,37 @@
 <template>
-	<Card class="h-64 flex flex-col min-w-64 transition-all duration-300 cursor-pointer shadow-md hover:shadow-xl">
-		<ThemeImage
-			class="w-full min-h-28 mb-3 rounded-t-md"
-			dark_src="/BackgroundDark.webp"
-			light_src="/BackgroundLight.webp"
-			alt="Ruehrstaat Squadron Banner Background"
-		/>
-		<CardContent>
-			<div class="flex justify-between items-center mt-auto">
-				<div class="text-lg font-semibold">
-					{{ carrier.name }} ({{ carrier.callsign }})
+	<i18n-link :to="{name: 'carrier-id', params: {id: carrier.id}}">
+		<Card
+			class="h-64 flex flex-col min-w-64 transition-all duration-300 cursor-pointer shadow-md hover:shadow-xl"
+		>
+			<ThemeImage
+				class="w-full min-h-28 mb-3 rounded-t-md"
+				dark_src="/BackgroundDark.webp"
+				light_src="/BackgroundLight.webp"
+				alt="Ruehrstaat Squadron Banner Background"
+			/>
+			<CardContent>
+				<div class="flex justify-between items-center mt-auto">
+					<div class="text-lg font-semibold">
+						{{ carrier.name }} ({{ carrier.callsign }})
+					</div>
+					<div class="ml-2">
+						<Badge>{{
+							$t("carrier.category." + carrier.category)
+						}}</Badge>
+					</div>
 				</div>
-				<div class="ml-2">
-					<Badge>{{
-						$t("carrier.category." + carrier.category)
-					}}</Badge>
+				<div class="text-sm mt-4">
+					<p>Current System: {{ carrier.currentLocation }}</p>
+					<p>
+						Docking Access:
+						{{
+							$t("carrier.dockingaccess." + carrier.dockingAccess)
+						}}
+					</p>
 				</div>
-			</div>
-			<div class="text-sm mt-4">
-				<p>Current System: {{ carrier.currentLocation }}</p>
-				<p>Docking Access: {{ $t("carrier.dockingaccess." + carrier.dockingAccess) }}</p>
-			</div>
-		</CardContent>
-	</Card>
+			</CardContent>
+		</Card>
+	</i18n-link>
 </template>
 
 <script lang="ts" setup>
